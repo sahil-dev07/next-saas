7c68fc853896fb91e37976cc837a0d9cc96ee199
Sahil <sahifindmyphone@gmail.com>
2024-03-09 12:19:41 +0530

    intial commit

---
+++ lib/Database/models/image.model.ts
@@ -0,0 +1,45 @@
+import { Document, Schema, model, models } from "mongoose";
+
+export interface IImage extends Document {
+    title: string;
+    transformationType: string;
+    publicId: string;
+    secureUrl: String;
+    width?: number;
+    height?: number;
+    config?: object;
+    transformationUrl?: String;
+    aspectRatio?: string;
+    color?: string;
+    prompt?: string;
+    author?: {
+        _id: String,
+        firstName: String,
+        lastName: String
+    }
+    createdAt?: Date;
+    updateAt?: Date;
+}
+
+
+const ImageSchema = new Schema({
+    title: { type: String, required: true },
+    transformationType: { type: String, required: true },
+    publicId: { type: String, required: true },
+    secureUrl: { type: URL, required: true },
+    width: { type: Number },
+    height: { type: Number },
+    config: { type: Object },
+    transformationUrl: { type: URL },
+    aspectRatio: { type: String },
+    color: { type: String },
+    prompt: { type: String },
+    author: { type: Schema.Types.ObjectId, ref: 'User' },
+    createdAt: { type: Date, default: Date.now },
+    updateAt: { type: Date, default: Date.now }
+})
+
+
+const Image = models?.Image || model("Image", ImageSchema);
+
+export default Image;
